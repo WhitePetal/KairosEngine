aca0cdff8944d018dc32f73839bc0bb827fa7b32
白翱翔 <baiaoxiang@wepie.com>
2026-08-07 17:41:37 +0800

    fix: bundles

--- KairosEngine/kairos_engine/src/ecs/bundle/info.rs
+++ KairosEngine/kairos_engine/src/ecs/bundle/info.rs
@@ -1,13 +1,18 @@
+use std::{any::TypeId, ptr::NonNull};
+
 use indexmap::{IndexMap, IndexSet};
 
 use crate::{
-    collections::FixedHashSet,
+    collections::{FixedHashMap, FixedHashSet, TypeIdMap},
     debug::{DebugCheckedUnwrap, MaybeLocation},
     ecs::{
-        archetype::{BundleComponentStatus, ComponentStatus},
-        bundle::DynamicBundle,
+        archetype::{Archetype, BundleComponentStatus, ComponentStatus},
+        bundle::{Bundle, DynamicBundle},
         change_detection::Tick,
-        component::{ComponentId, Components, RequiredComponentConstructor, StorageType},
+        component::{
+            ComponentId, Components, ComponentsRegistrator, RequiredComponentConstructor,
+            StorageType,
+        },
         entity::Entity,
         storage::{SparseSetIndex, SparseSets, Storages, Table, TableRow},
     },
@@ -73,10 +78,16 @@ pub struct BundleInfo {
 }
 
 impl BundleInfo {
+    /// Create a new [`BundleInfo`].
+    ///
+    /// # Safety
+    ///
+    /// Every ID in `component_ids` must be valid within the World that owns the `BundleInfo`
+    /// and must be in the same order as the source bundle type writes its components in.
     unsafe fn new(
         bundle_type_name: &'static str,
         storages: &mut Storages,
-        components: &mut Components,
+        components: &Components,
         mut component_ids: Vec<ComponentId>,
         id: BundleId,
     ) -> BundleInfo {
@@ -336,4 +347,229 @@ impl BundleInfo {
             }
         }
     }
+}
+
+/// The type of archetype move (or lack thereof) that will result from a bundle
+/// being inserted into an entity.
+pub(crate) enum ArchetypeMoveType {
+    /// If the entity already has all of the components that are being inserted,
+    /// its archetype won't change.
+    SameArchetype,
+    /// If only [`sparse set`](StorageType::SparseSet) components are being added,
+    /// the entity's archetype will change while keeping the same table.
+    NewArchetypeSameTable { new_archetype: NonNull<Archetype> },
+    /// If any [`table-stored`](StorageType::Table) components are being added,
+    /// both the entity's archetype and table will change.
+    NewArchetypeNewTable { new_archetype: NonNull<Archetype> },
+}
+
+/// Metadata for bundles. Stores a [`BundleInfo`] for each type of [`Bundle`] in a given world.
+#[derive(Default)]
+pub struct Bundles {
+    bundle_infos: Vec<BundleInfo>,
+    /// Cache static [`BundleId`]
+    bundle_ids: TypeIdMap<BundleId>,
+    /// Cache bundles, which contains both explicit and required components of [`Bundle`]
+    contributed_bundle_ids: TypeIdMap<BundleId>,
+    /// Cache dynamic [`BundleId`] with multiple components
+    dynamic_bundle_ids: FixedHashMap<Box<[ComponentId]>, BundleId>,
+    dynamic_bundle_storages: FixedHashMap<BundleId, Vec<StorageType>>,
+    /// Cache optimized dynamic [`BundleId`] with single component
+    dynamic_component_bundle_ids: FixedHashMap<ComponentId, BundleId>,
+    dynamic_component_storages: FixedHashMap<BundleId, StorageType>,
+}
+
+impl Bundles {
+    /// The total number of [`Bundle`] registered in [`Storages`].
+    pub fn len(&self) -> usize {
+        self.bundle_infos.len()
+    }
+
+    /// Returns true if no [`Bundle`] registered in [`Storages`].
+    pub fn is_empty(&self) -> bool {
+        self.len() == 0
+    }
+
+    /// Iterate over [`BundleInfo`].
+    pub fn iter(&self) -> impl Iterator<Item = &BundleInfo> {
+        self.bundle_infos.iter()
+    }
+
+    /// Gets the metadata associated with a specific type of bundle.
+    /// Returns `None` if the bundle is not registered with the world.
+    #[inline]
+    pub fn get(&self, bundle_id: BundleId) -> Option<&BundleInfo> {
+        self.bundle_infos.get(bundle_id.index())
+    }
+
+    /// Gets the value identifying a specific type of bundle.
+    /// Returns `None` if the bundle does not exist in the world,
+    /// or if `type_id` does not correspond to a type of bundle.
+    #[inline]
+    pub fn get_id(&self, type_id: TypeId) -> Option<BundleId> {
+        self.bundle_ids.get(&type_id).cloned()
+    }
+
+    /// Registers a new [`BundleInfo`] for a statically known type.
+    ///
+    /// Also registers all the components in the bundle.
+    ///
+    /// # Safety
+    ///
+    /// `components` and `storages` must be from the same [`World`] as `self`.
+    ///
+    /// [`World`]: crate::world::World
+    #[deny(unsafe_op_in_unsafe_fn)]
+    pub(crate) unsafe fn register_info<T: Bundle>(
+        &mut self,
+        components: &mut ComponentsRegistrator,
+        storages: &mut Storages,
+    ) -> BundleId {
+        let bundle_infos = &mut self.bundle_infos;
+        *self.bundle_ids.entry(TypeId::of::<T>()).or_insert_with(|| {
+            let component_ids = T::component_ids(components).collect::<Vec<_>>();
+            let id = BundleId(bundle_infos.len());
+            // SAFETY: T::component_id ensures:
+            // - its info was created
+            // - appropriate storage for it has been initialized.
+            // - it was created in the same order as the components in T
+            let bundle_info = unsafe {
+                BundleInfo::new(
+                    std::any::type_name::<T>(),
+                    storages,
+                    components,
+                    component_ids,
+                    id,
+                )
+            };
+            bundle_infos.push(bundle_info);
+            id
+        })
+    }
+
+    /// Registers a new [`BundleInfo`], which contains both explicit and required components for a statically known type.
+    ///
+    /// Also registers all the components in the bundle.
+    ///
+    /// # Safety
+    ///
+    /// `components` and `storages` must be from the same [`World`] as `self`.
+    ///
+    /// [`World`]: crate::world::World
+    #[deny(unsafe_op_in_unsafe_fn)]
+    pub(crate) unsafe fn register_contributed_bundle_info<T: Bundle>(
+        &mut self,
+        components: &mut ComponentsRegistrator,
+        storages: &mut Storages,
+    ) -> BundleId {
+        if let Some(id) = self.contributed_bundle_ids.get(&TypeId::of::<T>()).cloned() {
+            id
+        } else {
+            // SAFETY: as per the guarantees of this function, components and
+            // storages are from the same world as self
+            let explicit_bundle_id = unsafe { self.register_info::<T>(components, storages) };
+
+            // SAFETY: reading from `explicit_bundle_id` and creating new bundle in same time. Its valid because bundle hashmap allow this
+            let id = unsafe {
+                let (ptr, len) = {
+                    // SAFETY: `explicit_bundle_id` is valid and defined above
+                    let contributed = self
+                        .get_unchecked(explicit_bundle_id)
+                        .contributed_components();
+                    (contributed.as_ptr(), contributed.len())
+                };
+                // SAFETY: this is sound because the contributed_components Vec for explicit_bundle_id will not be accessed mutably as
+                // part of init_dynamic_info. No mutable references will be created and the allocation will remain valid.
+                self.init_dynamic_info(storages, components, std::slice::from_raw_parts(ptr, len))
+            };
+            self.contributed_bundle_ids.insert(TypeId::of::<T>(), id);
+            id
+        }
+    }
+
+    /// # Safety
+    /// A [`BundleInfo`] with the given [`BundleId`] must have been initialized for this instance of `Bundles`.
+    pub(crate) unsafe fn get_unchecked(&self, id: BundleId) -> &BundleInfo {
+        self.bundle_infos.get_unchecked(id.0)
+    }
+
+    /// # Safety
+    /// This [`BundleId`] must have been initialized with a single [`Component`](crate::component::Component)
+    /// (via [`init_component_info`](Self::init_dynamic_info))
+    pub(crate) unsafe fn get_storage_unchecked(&self, id: BundleId) -> StorageType {
+        *self
+            .dynamic_component_storages
+            .get(&id)
+            .debug_checked_unwrap()
+    }
+
+    /// # Safety
+    /// This [`BundleId`] must have been initialized with multiple [`Component`](crate::component::Component)s
+    /// (via [`init_dynamic_info`](Self::init_dynamic_info))
+    pub(crate) unsafe fn get_storages_unchecked(&mut self, id: BundleId) -> &mut Vec<StorageType> {
+        self.dynamic_bundle_storages
+            .get_mut(&id)
+            .debug_checked_unwrap()
+    }
+
+    /// Initializes a new [`BundleInfo`] for a dynamic [`Bundle`].
+    ///
+    /// # Panics
+    ///
+    /// Panics if any of the provided [`ComponentId`]s do not exist in the
+    /// provided [`Components`].
+    pub(crate) fn init_dynamic_info(
+        &mut self,
+        storages: &mut Storages,
+        components: &Components,
+        component_ids: &[ComponentId],
+    ) -> BundleId {
+        let bundle_infos = &mut self.bundle_infos;
+
+        // Use `raw_entry_mut` to avoid cloning `component_ids` to access `Entry`
+        let (_, bundle_id) = self
+            .dynamic_bundle_ids
+            .raw_entry_mut()
+            .from_key(component_ids)
+            .or_insert_with(|| {
+                let (id, storages) = initialize_dynamic_bundle(
+                    bundle_infos,
+                    storages,
+                    components,
+                    Vec::from(component_ids),
+                );
+                // SAFETY: The ID always increases when new bundles are added, and so, the ID is unique.
+                unsafe {
+                    self.dynamic_bundle_storages
+                        .insert_unique_unchecked(id, storages);
+                }
+                (component_ids.into(), id)
+            });
+        *bundle_id
+    }
+}
+
+/// Asserts that all components are part of [`Components`]
+/// and initializes a [`BundleInfo`].
+fn initialize_dynamic_bundle(
+    bundle_infos: &mut Vec<BundleInfo>,
+    storages: &mut Storages,
+    components: &Components,
+    component_ids: Vec<ComponentId>,
+) -> (BundleId, Vec<StorageType>) {
+    let storage_types = component_ids.iter().map(|&id| {
+        components.get_info(id).unwrap_or_else(|| {
+            panic!(
+                "init_dynamic_info called with component id {id:?} which doesn't exist in this world"
+            )
+        }).storage_type()
+    }).collect();
+
+    let id = BundleId(bundle_infos.len());
+    // SAFETY: `component_ids` are valid as they were just checked
+    let bundle_info =
+        unsafe { BundleInfo::new("<dynamic bundle>", storages, components, component_ids, id) };
+    bundle_infos.push(bundle_info);
+
+    (id, storage_types)
 }
