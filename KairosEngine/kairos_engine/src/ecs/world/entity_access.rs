171530de0723575981dab65245ab075d42ea2fc1
白翱翔 <baiaoxiang@wepie.com>
2026-08-20 10:08:42 +0800

    feat: entity_access except

--- KairosEngine/kairos_engine/src/ecs/world/entity_access.rs
+++ KairosEngine/kairos_engine/src/ecs/world/entity_access.rs
@@ -1,11 +1,13 @@
 mod component_fetch;
 mod entity_mut;
 mod entity_ref;
+mod except;
 mod filtered;
 mod world_mut;
 
 pub use component_fetch::*;
 pub use entity_mut::*;
 pub use entity_ref::*;
+pub use except::*;
 pub use filtered::*;
 pub use world_mut::*;
